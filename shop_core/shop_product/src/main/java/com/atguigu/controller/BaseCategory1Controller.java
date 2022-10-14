b0173304bca8b05feba34fc532f229e131708668
chenxiaohu <chen2635610020@163.com>
2022-10-14 20:35:23 +0800

    初始化搭建

---
+++ shop_core/shop_product/src/main/java/com/atguigu/controller/BaseCategory1Controller.java
@@ -0,0 +1,21 @@
+package com.atguigu.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 一级分类表 前端控制器
+ * </p>
+ *
+ * @author zhangqiang
+ * @since 2022-08-25
+ */
+@RestController
+@RequestMapping("/base-category1")
+public class BaseCategory1Controller {
+
+}
+
---
+++ shop_core/shop_product/src/main/java/com/atguigu/controller/BaseCategory2Controller.java
@@ -0,0 +1,21 @@
+package com.atguigu.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 二级分类表 前端控制器
+ * </p>
+ *
+ * @author zhangqiang
+ * @since 2022-08-25
+ */
+@RestController
+@RequestMapping("/base-category2")
+public class BaseCategory2Controller {
+
+}
+
